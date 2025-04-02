ecf0cf181de0289d0cac07ed6df314dc1b177018
Heath Woodruff <happyheathbar@gmail.com>
2025-04-02 14:09:27 -0600

    basic functionality

---
+++ client/src/Main.java
@@ -0,0 +1,8 @@
+import userExperience.GameRepl;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Guess the Word!");
+        new GameRepl().run();
+    }
+}
---
+++ out/production/fullStackCodeChallenge/README.md
@@ -0,0 +1,2 @@
+# fullStackCodeChallenge
+A word game using java and a word API. Written as part of a code challenge for a fullstack job position.
